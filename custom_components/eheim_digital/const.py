82f0ee218e592c7e1eaa900a32af986284e0f839
David <59320226+davidm-glitch@users.noreply.github.com>
2023-06-26 14:14:11 +0200

    Update const.py

--- custom_components/eheim_digital/const.py
+++ custom_components/eheim_digital/const.py
@@ -1,3 +1,8 @@
 """Constants for the EHEIM Digital integration."""
+import logging
 
 DOMAIN = "eheim_digital"
+DEFAULT_PORT = 80
+RESPONSE_TIMEOUT = 10
+CONF_DEVICES = "devices"
+_LOGGER = logging.getLogger(__name__)
