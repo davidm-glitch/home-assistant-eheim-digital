ee4ac4a6396d6a7c64563e3cb00b258c75f0391f
David <59320226+davidm-glitch@users.noreply.github.com>
2023-06-29 07:14:15 +0200

    Delete custom_components/eheim_digital directory

--- custom_components/eheim_digital/const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for the EHEIM Digital integration."""
-import logging
-
-DOMAIN = "eheim_digital"
-DEFAULT_PORT = 80
-RESPONSE_TIMEOUT = 10
-CONF_DEVICES = "devices"
-_LOGGER = logging.getLogger(__name__)
