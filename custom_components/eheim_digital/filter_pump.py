d93d86345388a8d3577cfdcaf3b4b309b622089f
jkhalil <jacquekhalil@gmail.com>
2023-08-12 12:30:06 -0700

    deleted files updated filter_pump.py

--- custom_components/eheim_digital/filter_pump.py
+++ custom_components/eheim_digital/filter_pump.py
@@ -1,22 +1,25 @@
 """Platform for filter integration"""
 
-from homeassistant.helpers.entity import Entity as FilterEntity
+#from homeassistant.helpers.entity import Entity
+from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.core import HomeAssistant
 from .coordinator import EheimDigitalDataUpdateCoordinator
 from homeassistant.config_entries import ConfigEntry
 
-from .const import LOGGER, DOMAIN, DEVICE_GROUPS, DEVICE_VERSIONS
+from .const import LOGGER, DOMAIN
 LOGGER.debug("Filter platform loaded")
 
-class EheimFilterDevice(Entity):
+class EheimFilterDevice(BinarySensorEntity):
     """Representation of an EHEIM filter"""
 
     def __init__(self, coordinator: EheimDigitalDataUpdateCoordinator, device) -> None:
         """Initialize the filter entity."""
+        self.device = device
         self.mac = device.from_mac
         self.coordinator = coordinator
         self._state = False
         self._filter_state = None
+
 
         LOGGER.debug("FilterEntity with MAC %s initialized", self.mac)
         LOGGER.debug("MAC Address: %s", self.mac)
@@ -38,13 +41,6 @@ class EheimFilterDevice(Entity):
         """Return the state of the Filter."""
         return self._state
 
-def get_device_group_for_version(version: int) -> str:
-    """Map version number to device group."""
-    for group, versions in DEVICE_GROUPS.items():
-        if DEVICE_VERSIONS[version] in versions:
-            return group
-    return None
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     """Set up the EHEIM Digital Filter platform from a config entry."""
     LOGGER.debug("Setting up EHEIM Digital Filter platform")
@@ -54,10 +50,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
     devices = coordinator.devices
 
     entities = []
-    for mac_address, device in devices.items():
-        if get_device_group_for_version(device.version) == "FILTER":
+    for device in devices:
+        device_mac = device.get("from")
+        if device.device_group == "FILTER_PUMP":
             entities.append(EheimFilterDevice(coordinator, device))
-            LOGGER.debug("Added EheimFilterDevice with MAC %s", mac_address)
+            LOGGER.debug("Added EheimFilterDevice with MAC %s", device_mac)
 
     LOGGER.debug("Adding Filter entities to Home Assistant")
     async_add_entities(entities)
